337bbb39512992f7d3f4d2c60981c6889649c23c
Gerundio98 <106779342+Gerundio98@users.noreply.github.com>
2022-11-11 18:34:20 -0400

    Update main.cpp
    Version corregida

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,19 +1,9 @@
-#include <iostream>
-
 int main() {
-    int contador=0;
     int cantidad=100;
     for(int i=cantidad-1; i>0; i--){
-        
         if((i%6)==0){
             if((i-8)%11==0){
-             contador=+1;
-            }
-            if(contador==1){
-                std::cout << "Salvador podría tener " << i;
-            }
-            else if(contador==2){
-               std::cout << " ó " << i <<"."; 
+                std::cout << "Sí salvador venido "<< 100-i<<" en la primera venta. Tendría al final" << i;
             }
         }
     }
