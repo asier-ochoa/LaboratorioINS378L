81747cbe7693bdc1a0c53c51cf12c94c5551c00c
Gerundio98 <106779342+Gerundio98@users.noreply.github.com>
2022-11-11 18:41:00 -0400

    Update main.cpp
    Ultimo de a deveras prometido

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -8,12 +8,13 @@
 
 //FECHA: 10/11/22
 
+#include <iostream>
 int main() {
-    int cantidad=100;
-    for(int i=cantidad-1; i>0; i--){
-        if((i%6)==0){
-            if((i-8)%11==0){
-                std::cout << "Sí salvador venido "<< 100-i<<" en la primera venta. Tendría al final" << i;
+    int cantidad = 100;
+    for (int i = cantidad - 1; i > 0; i--) {
+        if ((i % 6) == 0) {
+            if ((i - 8) % 11 == 0) {
+                std::cout << "Si salvador vendio " << 100 - i << " en la primera venta. Tendria al final " << i-8 << ".\n";
             }
         }
     }
