69ce4ff3e26fce8845996cb4b57155961cd1cc77
Gerundio98 <106779342+Gerundio98@users.noreply.github.com>
2022-11-11 18:21:26 -0400

    Update main.cpp
    Mi versión de los floreros

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hey rodolfo\n";
+    int contador=0;
+    int cantidad=100;
+    for(int i=cantidad-1; i>0; i--){
+        
+        if((i%6)==0){
+            if((i-8)%11==0){
+             contador=+1;
+            }
+            if(contador==1){
+                std::cout << "Salvador podría tener " << i;
+            }
+            else if(contador==2){
+               std::cout << " ó " << i <<"."; 
+            }
+        }
+    }
     return 0;
 }
