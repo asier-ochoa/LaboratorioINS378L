bf032589a72ca3403b7a7e8d0620923de0926c72
smugtwingo <smugtwingo>
2022-11-11 01:54:06 -0400

    I think this is a solution

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <cstdio>
 
 int main() {
-    std::cout << "Hey nigga\n";
+    int flowers = 100;
+    int rows = 6;
+
+    int soldNow;
+    for (int i = flowers / rows; i > 0; i--){
+        soldNow = flowers - i * rows;
+        printf("If %d flowers per row, %d were sold", i, soldNow);
+        soldNow += 8;
+        if ((flowers - soldNow) % 11 == 0)
+            printf(" | yup, he has %d flowers\n", flowers - soldNow);
+        else
+            printf(" | FUCK YOU, %d are left out\n", (flowers - soldNow) % 11);
+    }
     return 0;
 }
