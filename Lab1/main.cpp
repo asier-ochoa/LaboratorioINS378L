37d2ea8432dc4b3939fed7a0b2289c13dd396265
smugtwingo <smugtwingo>
2022-11-11 20:13:02 -0400

    fixed missing name

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -2,7 +2,7 @@
 //Hace algún tiempo Salvador compró 100 floreros para venderlos en un mercadillo. La semana pasada, después de haber vendido algunos de ellos en los días anteriores, tenía los floreros ordenados en seis filas, con el mismo número de unidades en cada una de ellas. En esta semana vendió ocho floreros y en la actualidad tiene los que le quedan ordenados en 11 filas, números con el mismo número de floreros en cada una.
 //¿Cuántos floreros tiene salvador para vender todavía?
 
-//PARTICIPANTES: 1101331 Ochoa Asier; 1072515 Rodoflo Peña; Carlos Ismael Garcia 1101629; 1100685 Miguelangel
+//PARTICIPANTES: 1101331 Ochoa Asier; 1072515 Rodoflo Peña; Carlos Ismael Garcia 1101629; 1100685 Miguelangel; Yeuris Terrero 1099399
 
 //FECHA: 10/11/22
 
