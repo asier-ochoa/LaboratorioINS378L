6db6b7036f5f11ac885c0d491dd5915940b499eb
Bletinor <yjtv@protonmail.com>
2022-11-14 14:43:16 -0400

    puse el enunciado

--- Teoria1/main.cpp
+++ Teoria1/main.cpp
@@ -1,3 +1,22 @@
+//ENUNCIADO:
+/*Realizar un programa C++ que lea un número real (correspondiente a un monto) y convertir este monto a palabra.
+
+Restricciones: 
+• El # tiene mascara 999,999,999.99 
+• Si el grupo determina puede variar el alcance del algoritmo. 
+• Solo llevar a palabras la parte entera del número. 
+• Utilizar el enfoque de divide y venceras 
+
+El programa debe desplegar en pantalla el resultado de la conversión del número a palabra. 
+Deben observarse todas las reglas gramaticales relativas a nombrar los numerales. Ejemplo: 
+si el usuario introduce 12,345.90 
+
+El programa debería desplegar Doce mil trescientos cuarenta y cinco con 90 centavos*/
+
+//PARTICIPANTES: 1101331 Ochoa Asier; 1072515 Rodoflo Peña; Carlos Ismael Garcia 1101629; 1100685 Miguelangel; Yeuris Terrero 1099399
+
+//FECHA: 18/11/22
+
 int main(){
     return 0;
 }
