be88451f935964f44abac14bdc0ab113b172b318
smugtwingo <smugtwingo>
2022-11-25 17:06:48 -0400

    goofy

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -8,17 +8,20 @@
 
 #include <cstdio>
 int main() {for (int i = 100 / 6; i > 0; i--) ((100 - ((100 - i * 6) + 8)) % 11 == 0 ? printf("If %d flowers per row, %d sold | Has %d flowers left\n", i, 100 - i * 6, 100 - ((100 - i * 6) + 8)) : 0);}
+//int main() {
 //    int flowers = 100;
 //    int rows = 6;
 //
 //    int soldNow;
-//    for (int i = flowers / rows; i > 0; i--){
+//    for (int i = flowers / rows; i > 0; i--) {
 //        soldNow = flowers - i * rows;
 //        printf("If %d flowers per row, %d were sold", i, soldNow);
 //        soldNow += 8;
 //        if ((flowers - soldNow) % 11 == 0)
 //            printf(" | yup, he has %d flowers\n", flowers - soldNow);
 //        else
-//            printf(" | nope, %d are left out\n", (flowers - soldNow) % 11);
+////            printf(" | nope, %d are left out\n", (flowers - soldNow) % 11);
+//            printf("\n");
 //    }
 //    return 0;
+//}
