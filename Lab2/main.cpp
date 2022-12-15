064320db12d41ec7a0f871c35f244dee22d478cc
Bletinor <yjtv@protonmail.com>
2022-12-14 20:28:03 -0400

    added files for lab2

---
+++ Lab2/main.cpp
@@ -0,0 +1,8 @@
+//ENUNCIADO: Cadena mas larga
+//PARTICIPANTES: 1101331 Asier Ochoa ; Yeuris Terrero | 1099399 ; Carlos Garcia | 1101629 ; Miguel Angel | 1100685 ; Rodolfo Peña 10712515
+//FECHA:19/05/22
+
+int main(){
+    
+    return 0;
+}
