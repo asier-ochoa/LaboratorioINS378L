596ecbbc73c788a80ff33a082ad65d6fe0e41963
Bletinor <yjtv@protonmail.com>
2023-01-12 18:54:37 -0400

    Project setup

--- LabDoc/entero.cpp
+++ LabDoc/entero.cpp
@@ -1,5 +1,4 @@
-//Escriba un programa C++, que dado un arreglo con múltiples valores, imprima la 
-//ocurrencia de cada elemento en el mismo.
+//Escriba un programa C++ que dado un número entero retorne los dígitos del número dado.
 //PARTICIPANTES: 1101331 Ochoa Asier; 1072515 Rodoflo Peña; Carlos Ismael Garcia 1101629; 1100685 Miguelangel; Yeuris Terrero 1099399
 
 #include<iostream>
---
+++ LabDoc/valorSup.cpp
@@ -0,0 +1,10 @@
+//Escriba un programa C++, que dado un arreglo con números, resuelva de manera eficiente la inquietud siguiente: ¿Cuál es el valor inmediatamente superior para un número en la posición i?, donde la distancia es la diferencia entre los índices del arreglo.
+//PARTICIPANTES: 1101331 Ochoa Asier; 1072515 Rodoflo Peña; Carlos Ismael Garcia 1101629; 1100685 Miguelangel; Yeuris Terrero 1099399
+
+#include<iostream>
+
+int main()
+{
+    
+    return 0;
+}
