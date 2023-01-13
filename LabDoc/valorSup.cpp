39e5d7e4c858ab0fa5d7fcfd418c7509adb0983d
Bletinor <yjtv@protonmail.com>
2023-01-13 17:32:15 -0400

    v1

--- LabDoc/valorSup.cpp
+++ LabDoc/valorSup.cpp
@@ -2,9 +2,31 @@
 //PARTICIPANTES: 1101331 Ochoa Asier; 1072515 Rodoflo Peña; Carlos Ismael Garcia 1101629; 1100685 Miguelangel; Yeuris Terrero 1099399
 
 #include<iostream>
+using namespace std;
 
 int main()
 {
+    //1 4 2 3 5 7
+    int size = 0, indx = 0;
+    cout << "Ingrese la longitud: ";
+    cin >> size;
+    int arr[size];
+
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << "Ingrese un valor: ";
+        cin >> arr[i];
+    }
+    
+    cout << "Cual indice quiere encontrar? ";
+    cin >> indx;
+    for (size_t i = 0; i < size; i++)
+    {
+        if (arr[i] == arr[indx] + 1)
+        {
+            cout << arr[i] << " es indmediatamente superior a " << arr[indx];
+        }
+    }
     
     return 0;
 }
