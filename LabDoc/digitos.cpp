877db52b4afdbfa591f7c0b4d2c45d2bed38bca9
smugtwingo <smugtwingo>
2023-01-13 20:46:34 -0400

    removed tildes

--- LabDoc/digitos.cpp
+++ LabDoc/digitos.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main()
 {
     string Respuesta="y", numstring;
-    int num=0, n, divisor, numnuevo, numposición;
+    int num=0, n, divisor, numnuevo, numposicion;
     bool respuestavalida = false, nodigito=true;
     
     while (Respuesta == "y") {
@@ -27,8 +27,8 @@ int main()
         }
         for (int i = 0; i < numstring.length(); i++) {
             try {
-                numposición = stoi(numstring.substr(i,1));
-                num = num + (numposición * pow(10, (numstring.length() - 1 - i)));
+                numposicion = stoi(numstring.substr(i,1));
+                num = num + (numposicion * pow(10, (numstring.length() - 1 - i)));
             }
             catch(exception){
                 nodigito = false;
@@ -50,8 +50,8 @@ int main()
             }
             for (int i = 0; i < numstring.length(); i++) {
                 try {
-                    numposición = stoi(numstring.substr(i,1));
-                    num = num + (numposición * pow(10, (numstring.length() - 1 - i)));
+                    numposicion = stoi(numstring.substr(i,1));
+                    num = num + (numposicion * pow(10, (numstring.length() - 1 - i)));
                 }
                 catch (exception) {
                     nodigito = false;
@@ -98,14 +98,4 @@ int main()
     }
     std::cout << "Fin del programa. \n";
 }
-    
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
